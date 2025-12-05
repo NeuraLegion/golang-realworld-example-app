09c6eb8cec11ffc517dc679b2c0b59be21a0ab52
Artem Derevnjuk <artedere@gmail.com>
2025-12-05 20:18:23 +0300

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-articles.test.ts
@@ -0,0 +1,51 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/articles', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['sqli', 'xss', 'csrf', 'bopla', 'secret_tokens'],
+      attackParamLocations: [AttackParamLocation.BODY, AttackParamLocation.HEADER],
+      starMetadata: {
+        code_source: "NeuraLegion/golang-realworld-example-app:main",
+        databases: ["MySQL"],
+        user_roles: { roles: [] }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/articles`,
+      body: {
+        article: {
+          title: "Sample Title",
+          description: "Sample Description",
+          body: "Sample body of the article.",
+          tagList: ["tag1", "tag2"]
+        }
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer <token>'
+      }
+    });
+});
---
+++ .brightsec/tests/post-api-users.test.ts
@@ -0,0 +1,47 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/users', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'email_injection', 'sqli', 'xss', 'secret_tokens'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'NeuraLegion/golang-realworld-example-app:main',
+        databases: ['MySQL'],
+        user_roles: { roles: [] }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/users`,
+      body: {
+        user: {
+          username: 'exampleuser',
+          email: 'user@example.com',
+          password: 'examplepassword'
+        }
+      },
+      headers: { 'Content-Type': 'application/json' }
+    });
+});
